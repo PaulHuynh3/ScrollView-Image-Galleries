42ec740d751d57a4b05d332903d30360b9321307
Paul <paul.huynh3@gmail.com>
2017-09-18 19:16:05 -0400

    add pictures and starting layout design

--- Scroll View Image Gallery/Scroll View Image Gallery/ViewController.m
+++ Scroll View Image Gallery/Scroll View Image Gallery/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollViewOutlet;
+@property (nonatomic) UIImageView *firstPicture;
+@property (nonatomic) UIImageView *secondPicture;
+@property (nonatomic) UIImageView *thirdPicture;
 
 @end
 
@@ -16,14 +20,56 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self createPictures];
+    [self setUpLayout];
+    
+    
+}
+
+-(void)createPictures{
+
+    //adding pictures to the scroll view.
+    self.firstPicture = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"toronto"]];
+    [self.scrollViewOutlet addSubview:self.firstPicture];
+    self.firstPicture.contentMode = UIViewContentModeScaleAspectFill;
+    self.firstPicture.clipsToBounds = YES;
+    
+    
+    self.secondPicture = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"chicago"]];
+    [self.scrollViewOutlet addSubview:self.secondPicture];
+    self.secondPicture.contentMode = UIViewContentModeScaleAspectFill;
+    self.secondPicture.clipsToBounds = YES;
+    
+    
+    self.thirdPicture = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"montreal"]];
+    [self.scrollViewOutlet addSubview:self.thirdPicture];
+    self.thirdPicture.contentMode = UIViewContentModeScaleAspectFill;
+    self.thirdPicture.clipsToBounds = YES;
+
+}
+
+-(void)setUpLayout{
+    self.firstPicture.translatesAutoresizingMaskIntoConstraints = NO;
+    
+
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+/*
+ - (void)setupLayout {
+ self.imageView1.translatesAutoresizingMaskIntoConstraints = NO;
+ self.imageView2.translatesAutoresizingMaskIntoConstraints = NO;
+ 
+ //set top and bottom anchors
+ [self.imageView1.topAnchor constraintEqualToAnchor:self.scrollView.topAnchor].active = YES;
+ [self.imageView1.bottomAnchor constraintEqualToAnchor:self.scrollView.bottomAnchor].active = YES;
+ 
+ */
 
+
+/*
+ In your view controller's -viewDidLoad method, create three UIImageView and place them inside the UIScrollView next to each other. Now would be a good time to consider your layout strategy: are you going to use explicit constraints, or set the frame and rely on translatesAutoresizingMaskIntoConstraints?
+ 
+ */
 
 @end
