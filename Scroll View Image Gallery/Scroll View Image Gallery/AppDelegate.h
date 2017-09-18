69fd959270d9f893c36dca34cff5eabe4d1cca41
Paul <paul.huynh3@gmail.com>
2017-09-18 18:28:34 -0400

    start

---
+++ Scroll View Image Gallery/Scroll View Image Gallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Scroll View Image Gallery
+//
+//  Created by Paul on 2017-09-18.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Scroll View Image Gallery/Scroll View Image Gallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Scroll View Image Gallery
+//
+//  Created by Paul on 2017-09-18.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
